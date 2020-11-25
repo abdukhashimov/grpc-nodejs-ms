e670d42dadd5aef5363f1bbb6497c4a7c6f98c20
Madiyor <abdukhashimov@yandex.ru>
2020-11-25 07:41:13 +0500

    added model

---
+++ models/hellotodo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const HelloSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const helloValidationSchema = Joi.object({
+  title: Joi.string().min(3).max(100).required(),
+  body: Joi.string().min(3).max(100).required(),
+});
+
+module.exports = helloValidationSchema;
+module.exports = HelloSchema;
